4d5cfc073f4420d4014f11e20b293f6d46347674
sanjay kumar <sanjayku6789@gmail.com>
2025-12-18 05:13:13 +0530

    fixed error of token

--- backend/controler/userControler.js
+++ backend/controler/userControler.js
@@ -63,8 +63,8 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+      secure: true,
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
