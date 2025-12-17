94281aa932df21b7534cc3483a08783152b4e49c
sanjay kumar <sanjayku6789@gmail.com>
2025-12-18 04:03:04 +0530

    fixed cors issue

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,10 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://mini-event-platform-gamma.vercel.app",
+    ],
     credentials: true,
   })
 );
