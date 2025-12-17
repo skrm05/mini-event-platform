c90294a17885e50917dbda9ff7f212bb2b1bb75f
sanjay kumar <sanjayku6789@gmail.com>
2025-12-18 04:18:25 +0530

    fixed url error

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,9 +3,8 @@ import axios from "axios";
 const isProduction = import.meta.env.MODE === "production";
 
 const baseURL = isProduction
-  ? "https://mini-event-platform-e21p.onrender.com/"
+  ? "https://mini-event-platform-e21p.onrender.com/api"
   : "http://localhost:9000/api";
-
 const api = axios.create({
   baseURL: baseURL,
   withCredentials: true,
