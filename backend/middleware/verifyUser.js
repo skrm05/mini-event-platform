084dd395d842fa541b7e8aa77026954286cb5e55
sanjay kumar <sanjayku6789@gmail.com>
2025-12-18 05:43:42 +0530

    console

--- backend/middleware/verifyUser.js
+++ backend/middleware/verifyUser.js
@@ -3,16 +3,19 @@ import { verifyToken } from "../util/jwt.js";
 export const verifyUser = async (req, res, next) => {
   try {
     let token = req.cookies.token;
+    console.log("token=> ", token);
     if (!token && req.headers.authorization) {
       token = req.headers.authorization.split(" ")[1];
     }
+
+    console.log("tokenHeaders=> ", token);
 
     if (!token) {
       return res.status(401).send({ error: "No Token Provided" });
     }
 
     let { id } = verifyToken(token);
-
+    console.log("id=> ", id);
     if (!id) {
       return res.status(401).send({ error: "Invalid Token" });
     } else {
