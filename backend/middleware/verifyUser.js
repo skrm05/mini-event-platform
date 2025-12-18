20f1b2ac28574e6539503d774d894dc9d2fa3831
sanjay kumar <sanjayku6789@gmail.com>
2025-12-18 05:38:45 +0530

    fixed error of cookies

--- backend/middleware/verifyUser.js
+++ backend/middleware/verifyUser.js
@@ -2,11 +2,17 @@ import { verifyToken } from "../util/jwt.js";
 
 export const verifyUser = async (req, res, next) => {
   try {
-    console.log("verify");
-    let { token } = req.cookies;
-    console.log("token=>", token);
+    let token = req.cookies.token;
+    if (!token && req.headers.authorization) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+
+    if (!token) {
+      return res.status(401).send({ error: "No Token Provided" });
+    }
+
     let { id } = verifyToken(token);
-    console.log("id=> ", id);
+
     if (!id) {
       return res.status(401).send({ error: "Invalid Token" });
     } else {
--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,13 +1,21 @@
 import axios from "axios";
 
-const isProduction = import.meta.env.MODE === "production";
-
-const baseURL = isProduction
-  ? "https://mini-event-platform-e21p.onrender.com/api"
-  : "http://localhost:9000/api";
 const api = axios.create({
-  baseURL: baseURL,
+  baseURL: "https://mini-event-platform-e21p.onrender.com//api",
   withCredentials: true,
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
