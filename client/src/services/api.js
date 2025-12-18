62d4cf2f83f2f4dea43fbddee396d9c3f241de44
sanjay kumar <sanjayku6789@gmail.com>
2025-12-18 05:59:39 +0530

    login update

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://mini-event-platform-e21p.onrender.com//api",
+  baseURL: "https://mini-event-platform-e21p.onrender.com/api",
   withCredentials: true,
 });
 
